40a05ab3662bd00eec8ead6b47a932e2df6e990f
mike <mike.janico@gmail.com>
2026-05-05 16:40:00 -0500

    a

--- agent/receptionist.js
+++ agent/receptionist.js
@@ -1,42 +1,12 @@
-"use strict";
-var __create = Object.create;
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-  // If the importer is in node compatibility mode or this is not an ESM
-  // file that has been converted to a CommonJS file using a Babel-
-  // compatible transform (i.e. "__esModule" has not been set), then set
-  // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-  mod
-));
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-var receptionist_exports = {};
-__export(receptionist_exports, {
-  default: () => receptionist_default
-});
-module.exports = __toCommonJS(receptionist_exports);
-var import_agents = require("@livekit/agents");
-var openai = __toESM(require("@livekit/agents-plugin-openai"), 1);
-var deepgram = __toESM(require("@livekit/agents-plugin-deepgram"), 1);
-var cartesia = __toESM(require("@livekit/agents-plugin-cartesia"), 1);
-var import_prisma = require("../src/lib/prisma");
-var receptionist_default = (0, import_agents.defineAgent)({
+import {
+  defineAgent,
+  voice
+} from "@livekit/agents";
+import * as openai from "@livekit/agents-plugin-openai";
+import * as deepgram from "@livekit/agents-plugin-deepgram";
+import * as cartesia from "@livekit/agents-plugin-cartesia";
+import { prisma } from "../src/lib/prisma";
+var receptionist_default = defineAgent({
   request_handler: async (req) => {
     console.log("--- Received Job Request ---");
     console.log("Job ID:", req.job.id);
@@ -66,7 +36,7 @@ var receptionist_default = (0, import_agents.defineAgent)({
       const withPlus = calledNumber.startsWith("+") ? calledNumber : "+" + calledNumber;
       const withoutPlus = calledNumber.replace("+", "");
       console.log("Looking up business for number:", calledNumber, "or", withPlus);
-      const user = await import_prisma.prisma.user.findFirst({
+      const user = await prisma.user.findFirst({
         where: {
           OR: [
             { AIPhone: calledNumber },
@@ -88,7 +58,7 @@ var receptionist_default = (0, import_agents.defineAgent)({
     } catch (e) {
       console.error("DB error during lookup:", e);
     }
-    const agent = new import_agents.voice.VoicePipelineAgent({
+    const agent = new voice.VoicePipelineAgent({
       stt: new deepgram.STT(),
       tts: new cartesia.TTS(),
       llm: new openai.LLM({
@@ -109,3 +79,6 @@ Your goal is to be helpful and professional, using the knowledge above to answer
     agent.say(`Hi, thanks for calling ${businessName}. This is Solomon!`);
   }
 });
+export {
+  receptionist_default as default
+};
